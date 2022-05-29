f78f557075a57a28f4f9cea1038871062606da9a
yanshanlangren <279659978@qq.com>
2022-05-29 15:54:19 +0800

    Update KafkaConsumerTest.java

--- src/main/test/java/kafka/KafkaConsumerTest.java
+++ src/main/test/java/kafka/KafkaConsumerTest.java
@@ -1,29 +1,41 @@
 package kafka;
 
-import lombok.extern.slf4j.Slf4j;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
+import org.slf4j.impl.StaticLoggerBinder;
 
 import java.util.*;
 
-@Slf4j
 public class KafkaConsumerTest {
 
     private static final String BOOTSTRAP_SERVERS = "i-oeruks5g:9092";
 
+    private Logger setLogLevel(){
+        LoggerContext loggerContext = (LoggerContext) StaticLoggerBinder.getSingleton().getLoggerFactory();
+        Logger logger = loggerContext.getLogger("ROOT");
+        logger.setLevel(Level.INFO);
+        return logger;
+    }
+
     @Test
     public void autoCommitConsumerTest() {
+        Logger logger = setLogLevel();
+
+
         Properties props = new Properties();
         props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
         props.put("group.id", "test");
         // 自动提交开关
         props.put("enable.auto.commit", "true");
         // 自动提交的时间间隔，此处是1s
-        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.commit.interval.ms", "3600000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
@@ -32,13 +44,15 @@ public class KafkaConsumerTest {
             // 调用poll后，1000ms后，消息状态会被改为 committed
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
-                System.out.println(record.toString());
+                logger.info(record.toString());
 //                insertIntoDB(record); // 将消息入库，时间可能会超过1000ms
         }
     }
 
     @Test
     public void manualCommitConsumerTest() {
+        Logger logger = setLogLevel();
+
         Properties props = new Properties();
         props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
         props.put("group.id", "test");
@@ -53,16 +67,19 @@ public class KafkaConsumerTest {
         List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
         while (true) {
             // 调用poll后，不会进行auto commit
-            ConsumerRecords<String, String> records = consumer.poll(100);
+            ConsumerRecords<String, String> records = consumer.poll(1000);
             for (ConsumerRecord<String, String> record : records) {
                 buffer.add(record);
             }
             if (buffer.size() >= minBatchSize) {
                 //TODO do something
-                System.out.println("buffer size["+buffer.size()+"] dealt");
+                for(ConsumerRecord<String, String> record : records){
+                    logger.info(record.toString());
+                }
+                logger.info("buffer size["+buffer.size()+"] dealt");
 //                insertIntoDb(buffer);
                 // 所有消息消费完毕以后，才进行commit操作
-                consumer.commitSync();
+//                consumer.commitSync();
                 buffer.clear();
             }
         }
@@ -70,6 +87,8 @@ public class KafkaConsumerTest {
 
     @Test
     public void manualCommitLowTest() {
+        Logger logger = setLogLevel();
+
         Properties props = new Properties();
         props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
         props.put("group.id", "test");
@@ -86,7 +105,7 @@ public class KafkaConsumerTest {
                 for (TopicPartition partition : records.partitions()) {
                     List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
                     for (ConsumerRecord<String, String> record : partitionRecords) {
-                        System.out.println(record.offset() + ": " + record.value());
+                        logger.info(record.offset() + ": " + record.value());
                     }
                     long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
                     // 精确控制offset
